39e58083a36a31b3cf656b45f51eeee048ec5280
Matbrobe <81379025+Matbrobe@users.noreply.github.com>
2021-04-29 21:44:09 +0200

    Grafiska grejer (#8)
    * fixed category buttons, comapany logo and text
    
    * fixade bollarna så att dom ligger rätt, kopplade sidorna, fifixade definitioner (Mathias, Viktor, Nils)
    
    Co-authored-by: Ralph-IT <57448988+Ralph-IT@users.noreply.github.com>
    Co-authored-by: malinros <54399138+malinros@users.noreply.github.com>

--- src/jsonDataLakarmottagning.js
+++ src/jsonDataLakarmottagning.js
@@ -2,16 +2,15 @@ export default {
     "categories": [
         {
             "title": "Audiometri",
-            "desc": "Vi är en privat läkarmottagning ansluten till västra götalandsregion. ",
+            "desc": "Hörseltest (inkl. läkarbedömning) 495 kr",
         },
         {
-            "title": "EKG",
-            "desc": "Vi är en privat läkarmottagning ansluten till västra götalandsregion. \nDu är välkommen att besöka oss oavsett vilken vårdcentral du är listad på. \nDet kostar 300 kr för läkarbesök 100 kr för distriktsköterskebesök barn under 20 år och äldre över 85 år gratis, frikort gäller.",
+            "title": "EKG (inkl. läkarbedömning)",
+            "desc": "350 kr",
         },
         {
-            "title": "Spirometri",
-            "desc": "jag heter malin",
-            "href" : "./IntygPage.html"
+            "title": "Spirometri (inkl. läkarbedömning)",
+            "desc": "795 kr"
         },
     ]
 }
