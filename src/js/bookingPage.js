2dcab5c59bcb6eff664a8cff3cc60edba86eebb9
malinros <54399138+malinros@users.noreply.github.com>
2021-05-06 09:32:52 +0200

    Booking frontend (#9)
    * Added a database folder with our server and booking functionality.
    
    * We created the elements that should be in the booking calendar in js. Kevin, Albin, Salvija, Malin
    
    * Now you can make a booking, Albin, Mathias, Kevin, Malin

--- src/js/bookingPage.js
+++ src/js/bookingPage.js
@@ -1,49 +1,110 @@
+
+let selectedBooking;
+
 window.onload = async () => {
-    const res = await fetch('http://localhost:3000/bookings', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-      },
-    })
+  const res = await fetch('http://localhost:3000/bookings', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+  })
 
-    const bookings = await res.json()
+  const bookings = await res.json()
 
-    const availableBookings = bookings.filter(booking => !booking.booked)
-  
-    // Samma gamla skit för att generara sakerna
+  const availableBookings = bookings.filter(booking => !booking.booked)
 
-    const mondayCont = document.getElementById("monday");
+  // Samma gamla skit för att generara sakerna
 
-    for (let index = 0; index < bookings.length; index++) {
-        const booking = bookings[index];
-        const bookingDiv = document.createElement("div");
-        //bookingDiv.className = "categories";
-        bookingDiv.id = booking.title;
-    
-        const bookingTitle = document.createElement("h2");
-        //bookingTitle.className = "category-name";
-    
-/*         const catImg = document.createElement("img");
-        catImg.src = "img\right_arrow.png";
-        catImg.className = "category"; */
-        
-    
-        bookingTitle.innerHTML = booking.title;
-        mondayCont.appendChild(bookingTitle);
-       // mondayCont.appendChild(catImg);
-    
-/*         if(category.href) {
-            catDiv.addEventListener("click", () => window.location.href = category.href)
-        } else {
-            catDiv.addEventListener("click", () => handleClick(category))
-        } */
-    
-        mondayCont.appendChild(bookingDiv);
+  const mondayCont = document.getElementById("monday");
+  const tuesdayCont = document.getElementById("tuesday");
+  const wednesdayCont = document.getElementById("wednesday");
+  const thursdayCont = document.getElementById("thursday");
+  const fridayCont = document.getElementById("friday");
+
+  for (let index = 0; index < availableBookings.length; index++) {
+    const booking = availableBookings[index];
+    const bookingDiv = document.createElement("div");
+    //bookingDiv.className = "categories";
+    bookingDiv.id = booking.title;
+
+    const bookingTitle = document.createElement("h2");
+    const bookingTime = document.createElement("p");
+
+    bookingTitle.innerHTML = booking.title;
+    bookingTime.innerHTML = booking.startTimeHour;
+
+    bookingDiv.appendChild(bookingTitle);
+    bookingDiv.appendChild(bookingTime);
+
+    if(booking.weekDay == "Monday"){
+
+      mondayCont.appendChild(bookingDiv);
+
+    } else if(booking.weekDay == "Tuesday"){
+
+      tuesdayCont.appendChild(bookingDiv);
+
+    } else if(booking.weekDay == "Wednesday"){
+
+      wednesdayCont.appendChild(bookingDiv);
+
+    } else if(booking.weekDay == "Thursday"){
+
+      thursdayCont.appendChild(bookingDiv);
+
+    } else if(booking.weekDay == "Friday"){
+
+      fridayCont.appendChild(bookingDiv);
+
     }
 
-
-
-
-
+    bookingDiv.addEventListener("click", () => handleClick(booking))
 
   }
+
+  document.getElementById("bookBtn").addEventListener("click", (e) => handleBook(e))
+
+}
+
+const handleBook = async (e) => {
+  e.preventDefault()
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("eMail");
+  const phoneNumber = document.getElementById("phoneNumber");
+  const comment = document.getElementById("comment");
+
+  selectedBooking.booked = true;
+  selectedBooking.firstName = firstName.value;
+  selectedBooking.lastName = lastName.value;
+  selectedBooking.email = email.value;
+  selectedBooking.phoneNumber = phoneNumber.value;
+  selectedBooking.comment = comment.value;
+
+  const res = await fetch('http://localhost:3000/booking', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(selectedBooking)
+  })
+  
+  if (res.status === 200) {
+    alert('Tid Bokad!')
+    window.location.reload()
+  }
+}
+
+const handleClick = (booking = undefined) => {
+
+  const popup = document.getElementById("myForm")
+
+  if (popup.className == "popup") {
+    popup.className = "popup hidden";
+  } else {
+    popup.className = "popup";
+  }
+
+  selectedBooking = booking;
+}
+
